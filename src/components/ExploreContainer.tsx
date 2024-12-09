081c0cf2636f39446d1ef5ab3ab1a4da26d99454
valevv <valeriavargasrojas@hotmail.es>
2024-12-09 18:17:25 +0100

    Initial commit including ionic first version project

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,13 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Catch Ching</strong>
+    </div>
+  );
+};
+
+export default ExploreContainer;
---
+++ src/pages/LessonQuiz1.xy.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {IonPage} from "@ionic/react";
+const LessonQuiz1: React.FC = () => {
+    return (
+        <div id="container">
+      <strong>Lesson Quiz 1</strong>
+    </div>
+    );
+};
+
+export default LessonQuiz1;
